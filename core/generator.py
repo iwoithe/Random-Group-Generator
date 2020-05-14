1c7fab640a50b9bdb8d170cad2417226a4823cf0
iwoithe <iwoithe@just42.net>
2020-05-14 20:48:22 +0930

    Generator code written and tested.

--- core/generator.py
+++ core/generator.py
@@ -23,15 +23,35 @@
 #
 
 import random
+from math import ceil
 
 def generate_groups(names, names_shuffled=False, num_groups=None, people_per_group=None):
-    if (people_per_group == None) and (num_groups != None):
-        people_per_group = get_people_per_group(num_groups)
+    ''' Generates the groups. The parameters are as follows:
+            names: the list of names
+            names_shuffled: this states whether or not the names
+                            list has been randomized or not
+            num_groups: the number of groups to generate
+            people_per_group: the number of people per group
+
+        The groups are generated in the following format and then
+        returned:
+            [[1, ['a', 'b', 'c']],
+             [2, ['d', 'e', 'f']],
+             [3, ['g', 'h']]
+        The first value is the group number (used more as a
+        reference when debugging), and the second value is
+        the people in the group.
+        '''
+
+    if (names_shuffled != True):
+        random.shuffle(names)
+
+    if (num_groups != None) and (people_per_group == None):
+        people_per_group = get_people_per_group(len(names), num_groups)
+    elif (num_groups == None) and (people_per_group != None):
+        pass
     else:
         people_per_group = 1
-
-    if !(names_shuffled):
-        random.shuffle()
 
     # In the future, see if using numpy arrays increase performance
     groups = []
@@ -40,21 +60,18 @@ def generate_groups(names, names_shuffled=False, num_groups=None, people_per_gro
     while len(names) > 0:
         people_names = []
         if len(names) > people_per_group:
-                people_names = names[:people_per_group]
-                del names[:people_per_group]
-                groups.append([group_num, people_names])
-                group_num += 1
-            else:
-                people_names = names.copy()
-                del names[:len(people_names)]
-                groups.append([group_num, people_names])
+            people_names = names[:people_per_group]
+            del names[:people_per_group]
+            groups.append([group_num, people_names])
+            group_num += 1
+        else:
+            people_names = names.copy()
+            del names[:len(people_names)]
+            groups.append([group_num, people_names])
 
-        return groups
+    return groups
 
 def get_people_per_group(num_people, num_groups):
-    people_per_group = int(num_people / num_groups) # Maybe a bug. If
-                                                    # there is an
-                                                    # incorrect number
-                                                    # of groups, try
-                                                    # rounding down.
+    ''' Converts the number of groups to people per group '''
+    people_per_group = ceil(num_people / num_groups)
     return people_per_group

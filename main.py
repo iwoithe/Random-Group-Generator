a54a006dd709a6c92433e0cce196242069cedaec
iwoithe <iwoithe@just42.net>
2020-05-14 20:44:26 +0930

    New file, save file and save file supported. About Qt also added.

--- main.py
+++ main.py
@@ -22,23 +22,120 @@
 #
 #
 
+import os
 import sys
 
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import (QApplication, QMainWindow)
+
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QKeySequence
+from PyQt5.QtWidgets import (QApplication, QMainWindow, QWidget,
+                             QHBoxLayout, QMenuBar, QFileDialog,
+                             QStyle)
+
+from ui.panels.editor import Editor
+from ui.panels.groups import Groups
+from ui.panels.options import Options
+
+import core.generator as generator
+
+# TODO: Check if a file has been modified and display an asterix
+#       (*) next to the filename
+# TODO: When closing, if a file has been modified, ask if you
+#       want to save the files
+# TODO: Add toggle features to toggle the views of some panes.
+#       Add toggle feature menu inside the view
+# TODO: Use panes (like Atoms) instead of using QDockWidget
 
 class RandomGroupGenerator(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.setupUi()
+        self.setup_ui()
 
-    def setupUi(self):
+    def setup_ui(self):
+        # Menu Bar
+        menu_bar = QMenuBar()
+        # File
+        action_file = menu_bar.addMenu("File")
+        action_file.addAction("New", self.new_file, QKeySequence.New)
+        action_file.addAction("Open", self.open_files, QKeySequence.Open)
+        action_file.addAction("Save", self.save_file, QKeySequence.Save)
+        action_file.addSeparator()
+        action_file.addAction("Quit", self.quit, QKeySequence.Quit)
+        # Edit
+        action_edit = menu_bar.addMenu("Edit")
+        action_edit.addAction("Preferences")
+        # View
+        action_view = menu_bar.addMenu("View")
+        # Help
+        action_help = menu_bar.addMenu("Help")
+        action_help.addAction("Manual")
+        action_help.addSeparator()
+        action_help.addAction("About")
+        action_help.addAction("About Qt", QApplication.instance().aboutQt)
+
+        # Editor
+        self.editor = Editor()
+
+        # Groups
+        groups = Groups()
+
+        # Options
+        options = Options(self.editor, groups, generator)
+
+        editor_dock = self.addDockWidget(Qt.LeftDockWidgetArea, self.editor)
+        options_dock = self.addDockWidget(Qt.RightDockWidgetArea, options)
+        groups_dock = self.addDockWidget(Qt.RightDockWidgetArea, groups)
 
         self.setWindowTitle("Random Group Generator")
+        self.setMenuBar(menu_bar)
+
+    def new_file(self):
+        self.editor.new_file()
+
+    def save_file(self):
+        open_files_path = ''
+        # Options for the open dialog
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+
+        # The variable _ is not used, hence its name
+        file_name, _ = QFileDialog.getSaveFileName(self,
+                "Save File",
+                "",
+                "Random Group Generator (*.rgg)", options=options)
+
+        if file_name:
+            if ('.rgg' in file_name):
+                pass
+            else:
+                file_name += ".rgg"
+
+            self.editor.save_file(file_name)
+
+    def open_files(self):
+        open_files_path = ''
+        # Options for the open dialog
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+
+        # The variable _ is not used, hence its name
+        files, _ = QFileDialog.getOpenFileNames(self,
+                "Open File/s", open_files_path,
+                "Random Group Generator Files (*.rgg)", options=options)
+
+        if files:
+            self.openFilesPath = files[0]
+            self.editor.load_files(files)
+
+    def quit(self):
+        self.destroy()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+    if sys.platform == 'win32':
+        app.setStyle("fusion")
     rgg = RandomGroupGenerator()
     rgg.show()
     sys.exit(app.exec_())
